fc1d82ab48b16682fa627dcd56f632866d8afd98
Oliver Marriott <hello@omarriott.com>
2021-07-24 00:22:40 +1000

    Hotpots all the way down

---
+++ dogfood.fnl
@@ -0,0 +1,95 @@
+;;
+;; This should be saved as lua/hotpot.lua
+;;
+
+(macro debug [...] `(print "dogfood ::" ,...))
+;; (macro debug [...])
+
+(local uv vim.loop)
+
+;; If this file is executing, we know it exists in the RTP so we can use this
+;; file to figure out related files needed for bootstraping.
+
+(var plugin-dir (-> :lua/hotpot.lua
+                    (vim.api.nvim_get_runtime_file false)
+                    (. 1)
+                    (uv.fs_realpath)
+                    (string.gsub :/lua/hotpot.lua$ "")))
+
+(local fnl-dir (.. plugin-dir :/fnl))
+(local cache-dir (-> :cache
+                     (vim.fn.stdpath)
+                     (.. :/hotpot/)))
+
+;; The canary file will tell us if we have compiled lua ready to
+;; run or if we need to compile first.
+(local canary (.. cache-dir fnl-dir :/hotpot.hotterpot.lua))
+(debug (.. "canary file: " canary))
+
+;; TODO: this shoud check if fnl is stale and remove the tree, force fennel
+;;       recomp.
+(if (vim.loop.fs_access canary :R)
+  ;; We have already complied the files, so just fiddle with luas
+  ;; package.path so we can run, undo the change since nvims rtp
+  ;; will handle any future needs
+  (let [old-package-path package.path]
+    (debug "load hotpot from cache")
+    (set package.path (.. cache-dir fnl-dir "/?.lua;" package.path))
+    (local hotpot (require :hotpot.hotterpot))
+    (set package.path old-package-path)
+    hotpot)
+
+  ;; The canary doesn't exist, so we have to manually load fennel,
+  ;; load Hotpot, then *hide* hotpot so hotpot can load hotpot
+  ;; and compile it out
+  (let [fennel (require :hotpot.fennel)]
+    (debug "compile hotpot")
+    ;; setup path so we can find hotpot to load it into memory
+    (local saved-fennel-path fennel.path)
+    (set fennel.path (.. fnl-dir "/?.fnl;" fennel.path))
+    (debug fennel.path)
+
+    ;; We want to isolate any fennel macros we find this time so
+    ;; they can be correctly seen again *next* time we require them.
+    ;; Without this, even when removing fennel from package.loaded, the macros
+    ;; would persist, meaning the dependecy would not be tracked correctly.
+    ;; Table.insert => push
+    (table.insert package.loaders (fennel.makeSearcher {:compilerEnv {}}))
+
+    ;; Load hotpot and set it up. It will now act on any future fnl files
+    (local hotpot (require :hotpot.hotterpot))
+    (hotpot.setup)
+
+    ;; Now we want to undo the changes we did to package.loaders, hide hotpot so
+    ;; we can require it again, and unload fennel too (at least what we can)
+    ;; Table.remove => pop
+    (table.remove package.loaders)
+    (each [name _ (pairs package.loaded)]
+      (if
+        (string.match name :^hotpot)
+        (do
+          (tset package.loaded (.. "_" name) (. package.loaded name))
+          (tset package.loaded name nil))
+        (string.match name :^fennel)
+        (do
+          (tset package.loaded name nil))))
+    (set fennel.path saved-fennel-path)
+
+    ;; Finnaly we can re-require hotpot, which will be seen by the resident
+    ;; hotpot, which will then compile and output into cache.
+    (debug "require hotpot.hotterpot")
+    (debug fennel.path)
+    (require :hotpot.hotterpot)
+
+    ;; now replace the last required hotpot version with our resident hotpot
+    ;; so future requires just return this copy
+;;    (each [name _ (pairs package.loaded)]
+;;      (if
+;;        (string.match name :^_hotpot)
+;;        (do
+;;          (tset package.loaded (string.gsub name "^_hotpot" "hotpot") (. package.loaded name)))
+;;          (tset package.loaded (.. "_" name) nil)))
+
+    ;; return our resident hotpot since it has already been setup
+    ;; and if we returned the new require, we could end up with two copies running.
+    hotpot))
--- lua/hotpot.lua
+++ lua/hotpot.lua
@@ -1,90 +1,95 @@
-local plugin_dir = vim.loop.fs_realpath(vim.api.nvim_get_runtime_file("lua/hotpot.lua", false)[1])
-plugin_dir = string.gsub(plugin_dir, "/lua/hotpot.lua$", "")
 
-local plugin_fnl_dir = plugin_dir .. "/fnl"
-local cache_dir = vim.fn.stdpath("cache") .. "/hotpot/"
-local canary = cache_dir .. plugin_fnl_dir .. "/hotpot2.lua"
-print("canary file: " .. canary)
 
--- if the canary exists, we already have a hotspot runtime waiting, we
--- just have to load it, then it will handle any future requests
-if vim.loop.fs_access(canary, "R") then
-  -- inject our cache dir into the start of lua's module search path
-  -- then require ourselves, then clean up as normally we will use vims
-  -- runtimepath
-  local save = package.path
-  package.path = cache_dir .. plugin_fnl_dir .. "/?.lua;" .. package.path
-  -- we can now just load hotpot like normal
-  local hotpot = require("hotpot2")
-  -- restore path because the hotpot searcher will takeover now
-  package.path = save
-  -- and we're done
-  return hotpot
-else
-  -- no cache exists, so we have to build one. We can do this by loading hotpot
-  -- directly via fennel, then asking hotpot to find itself, which will
-  -- generate the cache.
-  local fennel = require("hotpot.fennel")
-  print(fennel.version)
-  fennel.path = plugin_fnl_dir .. "/?.fnl;" .. fennel.path
---  fennel.dofile(plugin_fnl_dir .. "/hotpot2.fnl", {
---    compilerEnv = {},
---    env = {
---      require = function(mod)
---        print(mod)
---      end,
---      vim = vim
---    }
---  })
 
-  -- insert searcher at head so it skips this file in favour of the fnl file
-  table.insert(package.loaders, fennel.makeSearcher({compilerEnv = {}})) -- inserts at the end
-  print(vim.inspect(package.loaders))
-  local hotpot = require("hotpot2")
-  hotpot.setup()
-  -- remove fennel searcher since we have our own,
-  -- which has been inserted at the front
-  table.remove(package.loaders) -- removes from end
 
-  -- transplant hotpot to _hotpot, so we can require("hotpot") to
-  -- properly compile ourselves out
-  for name, _ in pairs(package.loaded) do
-    if string.match(name, "^hotpot") then
-      package.loaded["_" .. name] = package.loaded[name]
-      package.loaded[name] = nil
-      print("package.loaded[" .. name .. "] => _" .. name)
-    elseif  string.match(name, "^fennel") then
-      print("dropping package: " .. name)
-      package.loaded[name] = nil
-    end
-  end
 
-  -- don't run setup
-  print("** start require(hotpot2)")
-  require("hotpot2")
-  print("** end require(hotpot2)")
 
-  return hotpot
-end
---
---  print(vim.inspect(package.loaders))
---  print("required hotpot from fennel")
---
---  package.loaded["hotpot2"] = nil
---  collectgarbage()
---
---  print("### cleared package.loaded")
---
---  -- wrap setup() so we require ourselves again
---  -- so we can start from a clean state
---  local _setup = hotpot.setup
---  hotpot.setup = function()
---    print("hijacked setup")
---    local val = _setup()
---    package.loaded["hotpot"] = nil
---    require("hotpot2")
---    return val
---  end
---
---  return hotpot
 
+ local uv = vim.loop
+
+
+
+
+
+
+
+
+ local plugin_dir = string.gsub(uv.fs_realpath((vim.api.nvim_get_runtime_file("lua/hotpot.lua", false))[1]), "/lua/hotpot.lua$", "")
+
+ local fnl_dir = (plugin_dir .. "/fnl")
+
+
+ local cache_dir = (vim.fn.stdpath("cache") .. "/hotpot/")
+
+
+
+ local canary = (cache_dir .. fnl_dir .. "/hotpot.hotterpot.lua") print("dogfood ::", ("canary file: " .. canary))
+
+
+
+
+ if vim.loop.fs_access(canary, "R") then
+
+
+
+ local old_package_path = package.path print("dogfood ::", "load hotpot from cache")
+
+ package.path = (cache_dir .. fnl_dir .. "/?.lua;" .. package.path)
+ local hotpot = require("hotpot.hotterpot")
+ package.path = old_package_path
+ return hotpot else
+
+
+
+
+ local fennel = require("hotpot.fennel") print("dogfood ::", "compile hotpot")
+
+
+ local saved_fennel_path = fennel.path
+ fennel.path = (fnl_dir .. "/?.fnl;" .. fennel.path) print("dogfood ::", fennel.path)
+
+
+
+
+
+
+
+ table.insert(package.loaders, fennel.makeSearcher({compilerEnv = {}}))
+
+
+ local hotpot = require("hotpot.hotterpot")
+ hotpot.setup()
+
+
+
+
+ table.remove(package.loaders)
+ for name, _ in pairs(package.loaded) do
+ if string.match(name, "^hotpot") then
+
+
+ package.loaded[("_" .. name)] = package.loaded[name]
+ package.loaded[name] = nil elseif string.match(name, "^fennel") then
+
+
+ package.loaded[name] = nil end end
+ fennel.path = saved_fennel_path print("dogfood ::", "require hotpot.hotterpot") print("dogfood ::", fennel.path)
+
+
+
+
+
+ require("hotpot.hotterpot")
+
+
+
+
+
+
+
+
+
+
+
+
+ return hotpot end
