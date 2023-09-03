83a60c076f6e47b74c246da434b1c31d67be2b0e
Oliver Marriott <hello@omarriott.com>
2023-09-03 23:56:15 +1000

    feat: test_in_container.sh <test-name.lua>

--- test/test_in_container.sh
+++ test/test_in_container.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-podman_command=${1:-podman}
-podman_flags=${2:-}
+# podman_command=${1:-podman}
+# podman_flags=${2:-}
+podman_command=podman
+podman_flags=""
+only_test=${1:-}
 start_at=$(date +%s)
 failed=0
 
@@ -11,7 +14,7 @@ test_image_id=$($podman_command build $podman_flags --quiet -f ./test/Containerf
 echo "Image id: $test_image_id"
 
 echo "Running tests in $test_image_id"
-$podman_command run --rm -it $test_image_id
+$podman_command run --rm -it $test_image_id test/test.sh $only_test
 failed=$?
 
 # finish_at=$(date +%s)
