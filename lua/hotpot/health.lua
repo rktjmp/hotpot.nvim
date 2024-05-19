7f53e1d4fdc907e808999c83ad3592e336398add
Oliver Marriott <hello@omarriott.com>
2024-05-19 20:53:24 +1000

    chore: bump lua build

--- lua/hotpot/health.lua
+++ lua/hotpot/health.lua
@@ -1,5 +1,20 @@
 local uv = (vim.uv or vim.loop)
-local _local_1_ = vim.health
+local function _3_(...)
+  local _2_ = vim.health
+  if ((_G.type(_2_) == "table") and (nil ~= _2_.ok) and (nil ~= _2_.info) and (nil ~= _2_.error) and (nil ~= _2_.start)) then
+    local ok = _2_.ok
+    local info = _2_.info
+    local error = _2_.error
+    local start = _2_.start
+    return {report_start = start, report_info = info, report_error = error, report_ok = ok}
+  elseif (nil ~= _2_) then
+    local other = _2_
+    return other
+  else
+    return nil
+  end
+end
+local _local_1_ = _3_(...)
 local report_start = _local_1_["report_start"]
 local report_info = _local_1_["report_info"]
 local report_ok = _local_1_["report_ok"]
@@ -11,78 +26,78 @@ local function bytes__3ehuman(bytes)
   local function f(b)
     return (b / 1024)
   end
-  local function _2_(...)
-    local _3_ = ...
-    local function _4_(...)
-      local bytes0 = _3_[1]
-      local unit = _3_[2]
+  local function _5_(...)
+    local _6_ = ...
+    local function _7_(...)
+      local bytes0 = _6_[1]
+      local unit = _6_[2]
       return (1023 < bytes0)
     end
-    if (((_G.type(_3_) == "table") and (nil ~= _3_[1]) and (nil ~= _3_[2])) and _4_(...)) then
-      local bytes0 = _3_[1]
-      local unit = _3_[2]
-      local function _5_(...)
-        local _6_ = ...
-        local function _7_(...)
-          local kbytes = _6_[1]
-          local unit0 = _6_[2]
+    if (((_G.type(_6_) == "table") and (nil ~= _6_[1]) and (nil ~= _6_[2])) and _7_(...)) then
+      local bytes0 = _6_[1]
+      local unit = _6_[2]
+      local function _8_(...)
+        local _9_ = ...
+        local function _10_(...)
+          local kbytes = _9_[1]
+          local unit0 = _9_[2]
           return (1023 < kbytes)
         end
-        if (((_G.type(_6_) == "table") and (nil ~= _6_[1]) and (nil ~= _6_[2])) and _7_(...)) then
-          local kbytes = _6_[1]
-          local unit0 = _6_[2]
-          local function _8_(...)
-            local _9_ = ...
-            local function _10_(...)
-              local mbytes = _9_[1]
-              local unit1 = _9_[2]
+        if (((_G.type(_9_) == "table") and (nil ~= _9_[1]) and (nil ~= _9_[2])) and _10_(...)) then
+          local kbytes = _9_[1]
+          local unit0 = _9_[2]
+          local function _11_(...)
+            local _12_ = ...
+            local function _13_(...)
+              local mbytes = _12_[1]
+              local unit1 = _12_[2]
               return (1023 < mbytes)
             end
-            if (((_G.type(_9_) == "table") and (nil ~= _9_[1]) and (nil ~= _9_[2])) and _10_(...)) then
-              local mbytes = _9_[1]
-              local unit1 = _9_[2]
-              local function _11_(...)
-                local _12_ = ...
-                if ((_G.type(_12_) == "table") and (nil ~= _12_[1]) and (nil ~= _12_[2])) then
-                  local gbytes = _12_[1]
-                  local unit2 = _12_[2]
+            if (((_G.type(_12_) == "table") and (nil ~= _12_[1]) and (nil ~= _12_[2])) and _13_(...)) then
+              local mbytes = _12_[1]
+              local unit1 = _12_[2]
+              local function _14_(...)
+                local _15_ = ...
+                if ((_G.type(_15_) == "table") and (nil ~= _15_[1]) and (nil ~= _15_[2])) then
+                  local gbytes = _15_[1]
+                  local unit2 = _15_[2]
                   return fmt(unit2, gbytes)
-                elseif ((_G.type(_12_) == "table") and (nil ~= _12_[1]) and (nil ~= _12_[2])) then
-                  local size = _12_[1]
-                  local unit2 = _12_[2]
+                elseif ((_G.type(_15_) == "table") and (nil ~= _15_[1]) and (nil ~= _15_[2])) then
+                  local size = _15_[1]
+                  local unit2 = _15_[2]
                   return fmt(unit2, size)
                 else
                   return nil
                 end
               end
-              return _11_({f(mbytes), "%.2fbg"})
-            elseif ((_G.type(_9_) == "table") and (nil ~= _9_[1]) and (nil ~= _9_[2])) then
-              local size = _9_[1]
-              local unit1 = _9_[2]
+              return _14_({f(mbytes), "%.2fbg"})
+            elseif ((_G.type(_12_) == "table") and (nil ~= _12_[1]) and (nil ~= _12_[2])) then
+              local size = _12_[1]
+              local unit1 = _12_[2]
               return fmt(unit1, size)
             else
               return nil
             end
           end
-          return _8_({f(kbytes), "%.2fmb"})
-        elseif ((_G.type(_6_) == "table") and (nil ~= _6_[1]) and (nil ~= _6_[2])) then
-          local size = _6_[1]
-          local unit0 = _6_[2]
+          return _11_({f(kbytes), "%.2fmb"})
+        elseif ((_G.type(_9_) == "table") and (nil ~= _9_[1]) and (nil ~= _9_[2])) then
+          local size = _9_[1]
+          local unit0 = _9_[2]
           return fmt(unit0, size)
         else
           return nil
         end
       end
-      return _5_({f(bytes0), "%dkb"})
-    elseif ((_G.type(_3_) == "table") and (nil ~= _3_[1]) and (nil ~= _3_[2])) then
-      local size = _3_[1]
-      local unit = _3_[2]
+      return _8_({f(bytes0), "%dkb"})
+    elseif ((_G.type(_6_) == "table") and (nil ~= _6_[1]) and (nil ~= _6_[2])) then
+      local size = _6_[1]
+      local unit = _6_[2]
       return fmt(unit, size)
     else
       return nil
     end
   end
-  return _2_({bytes, "%db"})
+  return _5_({bytes, "%db"})
 end
 local function disk_info()
   report_start("Hotpot Cache Data")
@@ -92,22 +107,22 @@ local function disk_info()
   local paths = vim.fn.globpath(cache_root, "**", true, true, true)
   local count = #paths
   local size
-  local function _17_()
+  local function _20_()
     local size0 = 0
     for _, p in ipairs(paths) do
-      local function _18_(...)
-        local t_19_ = uv.fs_stat(p)
-        if (nil ~= t_19_) then
-          t_19_ = t_19_.size
+      local function _21_(...)
+        local t_22_ = uv.fs_stat(p)
+        if (nil ~= t_22_) then
+          t_22_ = t_22_.size
         else
         end
-        return t_19_
+        return t_22_
       end
-      size0 = (size0 + _18_() + 0)
+      size0 = (size0 + _21_() + 0)
     end
     return size0
   end
-  size = bytes__3ehuman(_17_())
+  size = bytes__3ehuman(_20_())
   report_info(fmt("Cache root path: %s", cache_root))
   return report_info(fmt("Cache size: %s files, %s", count, size))
 end
@@ -117,11 +132,11 @@ local function log_info()
   local path = logger.path()
   local size
   do
-    local _21_ = uv.fs_stat(path)
-    if (_21_ == nil) then
+    local _24_ = uv.fs_stat(path)
+    if (_24_ == nil) then
       size = 0
-    elseif ((_G.type(_21_) == "table") and (nil ~= _21_.size)) then
-      local size0 = _21_.size
+    elseif ((_G.type(_24_) == "table") and (nil ~= _24_.size)) then
+      local size0 = _24_.size
       size = bytes__3ehuman(size0)
     else
       size = nil
@@ -132,8 +147,8 @@ local function log_info()
 end
 local function searcher_info()
   report_start("Hotpot Module Searcher")
-  local _let_23_ = require("hotpot.loader")
-  local searcher = _let_23_["searcher"]
+  local _let_26_ = require("hotpot.loader")
+  local searcher = _let_26_["searcher"]
   local expected_index = 2
   local actual_index
   do
--- lua/hotpot/loader/init.lua
+++ lua/hotpot/loader/init.lua
@@ -23,24 +23,26 @@ local function needs_compilation_3f(record)
   local _let_6_ = record
   local lua_path = _let_6_["lua-path"]
   local files = _let_6_["files"]
-  local function lua_missing_3f()
-    return file_missing_3f(record["lua-path"])
+  local lua_missing_3f
+  local function _7_()
+    return file_missing_3f(lua_path)
   end
+  lua_missing_3f = _7_
   local function files_changed_3f()
     local stale_3f = false
     for _, historic_file in ipairs(files) do
       if stale_3f then break end
-      local _let_7_ = historic_file
-      local path = _let_7_["path"]
-      local historic_size = _let_7_["size"]
-      local _let_8_ = _let_7_["mtime"]
-      local hsec = _let_8_["sec"]
-      local hnsec = _let_8_["nsec"]
-      local _let_9_ = file_stat(path)
-      local current_size = _let_9_["size"]
-      local _let_10_ = _let_9_["mtime"]
-      local csec = _let_10_["sec"]
-      local cnsec = _let_10_["nsec"]
+      local _let_8_ = historic_file
+      local path = _let_8_["path"]
+      local historic_size = _let_8_["size"]
+      local _let_9_ = _let_8_["mtime"]
+      local hsec = _let_9_["sec"]
+      local hnsec = _let_9_["nsec"]
+      local _let_10_ = file_stat(path)
+      local current_size = _let_10_["size"]
+      local _let_11_ = _let_10_["mtime"]
+      local csec = _let_11_["sec"]
+      local cnsec = _let_11_["nsec"]
       stale_3f = ((historic_size ~= current_size) or (hsec ~= csec) or (hnsec ~= cnsec))
     end
     return stale_3f
@@ -48,68 +50,68 @@ local function needs_compilation_3f(record)
   return (lua_missing_3f() or files_changed_3f() or false)
 end
 local function record_loadfile(record)
-  local _let_11_ = require("hotpot.lang.fennel.compiler")
-  local compile_record = _let_11_["compile-record"]
-  local _let_12_ = require("hotpot.runtime")
-  local config_for_context = _let_12_["config-for-context"]
-  local _let_13_ = config_for_context((record["sigil-path"] or record["src-path"]))
-  local compiler = _let_13_["compiler"]
-  local _let_14_ = compiler
-  local modules_options = _let_14_["modules"]
-  local macros_options = _let_14_["macros"]
-  local preprocessor = _let_14_["preprocessor"]
+  local _let_12_ = require("hotpot.lang.fennel.compiler")
+  local compile_record = _let_12_["compile-record"]
+  local _let_13_ = require("hotpot.runtime")
+  local config_for_context = _let_13_["config-for-context"]
+  local _let_14_ = config_for_context((record["sigil-path"] or record["src-path"]))
+  local compiler = _let_14_["compiler"]
+  local _let_15_ = compiler
+  local modules_options = _let_15_["modules"]
+  local macros_options = _let_15_["macros"]
+  local preprocessor = _let_15_["preprocessor"]
   if needs_compilation_3f(record) then
-    local function _15_(...)
-      local _16_, _17_ = ...
-      if ((_16_ == true) and (nil ~= _17_)) then
-        local deps = _17_
-        local function _18_(...)
-          local _19_, _20_ = ...
-          if (nil ~= _19_) then
-            local record0 = _19_
-            local function _21_(...)
-              local _22_, _23_ = ...
-              if (nil ~= _22_) then
-                local record1 = _22_
+    local function _16_(...)
+      local _17_, _18_ = ...
+      if ((_17_ == true) and (nil ~= _18_)) then
+        local deps = _18_
+        local function _19_(...)
+          local _20_, _21_ = ...
+          if (nil ~= _20_) then
+            local record0 = _20_
+            local function _22_(...)
+              local _23_, _24_ = ...
+              if (nil ~= _23_) then
+                local record1 = _23_
                 return loadfile(record1["lua-path"])
-              elseif ((_22_ == false) and (nil ~= _23_)) then
-                local e = _23_
+              elseif ((_23_ == false) and (nil ~= _24_)) then
+                local e = _24_
                 local msg = fmt("\nHotpot could not compile the file `%s`:\n\n%s", record0["src-path"], e)
                 return error(msg, 0)
               else
-                local _ = _22_
+                local _ = _23_
                 return nil
               end
             end
-            return _21_(save_record(record0))
-          elseif ((_19_ == false) and (nil ~= _20_)) then
-            local e = _20_
+            return _22_(save_record(record0))
+          elseif ((_20_ == false) and (nil ~= _21_)) then
+            local e = _21_
             local msg = fmt("\nHotpot could not compile the file `%s`:\n\n%s", record["src-path"], e)
             return error(msg, 0)
           else
-            local _ = _19_
+            local _ = _20_
             return nil
           end
         end
-        return _18_(set_record_files(record, deps))
-      elseif ((_16_ == false) and (nil ~= _17_)) then
-        local e = _17_
+        return _19_(set_record_files(record, deps))
+      elseif ((_17_ == false) and (nil ~= _18_)) then
+        local e = _18_
         local msg = fmt("\nHotpot could not compile the file `%s`:\n\n%s", record["src-path"], e)
         return error(msg, 0)
       else
-        local _ = _16_
+        local _ = _17_
         return nil
       end
     end
-    return _15_(compile_record(record, modules_options, macros_options, preprocessor))
+    return _16_(compile_record(record, modules_options, macros_options, preprocessor))
   else
     return loadfile(record["lua-path"])
   end
 end
 local function handle_cache_lua_path(lua_path_in_cache)
-  local _28_ = fetch_record(lua_path_in_cache)
-  if (nil ~= _28_) then
-    local record = _28_
+  local _29_ = fetch_record(lua_path_in_cache)
+  if (nil ~= _29_) then
+    local record = _29_
     if (file_exists_3f(record["src-path"]) and file_missing_3f(record["lua-colocation-path"])) then
       return record_loadfile(record)
     else
@@ -117,7 +119,7 @@ local function handle_cache_lua_path(lua_path_in_cache)
       drop_record(record)
       return REPEAT_SEARCH
     end
-  elseif (_28_ == nil) then
+  elseif (_29_ == nil) then
     rm_file(lua_path_in_cache)
     return REPEAT_SEARCH
   else
@@ -126,28 +128,28 @@ local function handle_cache_lua_path(lua_path_in_cache)
 end
 local function find_module(modname)
   local function search_by_existing_lua(modname0)
-    local _31_ = vim.loader.find(modname0)
-    if ((_G.type(_31_) == "table") and ((_G.type(_31_[1]) == "table") and (nil ~= _31_[1].modpath))) then
-      local found_lua_path = _31_[1].modpath
+    local _32_ = vim.loader.find(modname0)
+    if ((_G.type(_32_) == "table") and ((_G.type(_32_[1]) == "table") and (nil ~= _32_[1].modpath))) then
+      local found_lua_path = _32_[1].modpath
       local cache_affix = cache_path_for_compiled_artefact()
       local make_loader
       do
-        local _32_ = string.find(found_lua_path, cache_affix, 1, true)
-        if (_32_ == 1) then
+        local _33_ = string.find(found_lua_path, cache_affix, 1, true)
+        if (_33_ == 1) then
           make_loader = handle_cache_lua_path
         else
-          local _ = _32_
+          local _ = _33_
           make_loader = loadfile
         end
       end
-      local _34_, _35_ = make_loader(found_lua_path)
-      if (_34_ == REPEAT_SEARCH) then
+      local _35_, _36_ = make_loader(found_lua_path)
+      if (_35_ == REPEAT_SEARCH) then
         return search_by_existing_lua(modname0)
       else
-        local _3floader = _34_
+        local _3floader = _35_
         return _3floader
       end
-    elseif ((_G.type(_31_) == "table") and (_31_[1] == nil)) then
+    elseif ((_G.type(_32_) == "table") and (_32_[1] == nil)) then
       return false
     else
       return nil
@@ -156,105 +158,105 @@ local function find_module(modname)
   local function search_by_rtp_fnl(modname0)
     local search_runtime_path
     do
-      local _let_38_ = require("hotpot.searcher")
-      local mod_search = _let_38_["mod-search"]
-      local function _39_(modname1)
+      local _let_39_ = require("hotpot.searcher")
+      local mod_search = _let_39_["mod-search"]
+      local function _40_(modname1)
         return mod_search({prefix = "fnl", extension = "fnl", modnames = {(modname1 .. ".init"), modname1}, ["package-path?"] = false})
       end
-      search_runtime_path = _39_
+      search_runtime_path = _40_
     end
-    local _40_ = search_runtime_path(modname0)
-    if ((_G.type(_40_) == "table") and (nil ~= _40_[1])) then
-      local src_path = _40_[1]
-      local function _41_(...)
-        local _42_ = ...
-        if (nil ~= _42_) then
-          local index = _42_
-          local function _43_(...)
-            local _44_ = ...
-            if (nil ~= _44_) then
-              local loader = _44_
-              local function _45_(...)
-                local _46_, _47_ = ...
-                if ((_46_ == false) and (nil ~= _47_)) then
-                  local e = _47_
+    local _41_ = search_runtime_path(modname0)
+    if ((_G.type(_41_) == "table") and (nil ~= _41_[1])) then
+      local src_path = _41_[1]
+      local function _42_(...)
+        local _43_ = ...
+        if (nil ~= _43_) then
+          local index = _43_
+          local function _44_(...)
+            local _45_ = ...
+            if (nil ~= _45_) then
+              local loader = _45_
+              local function _46_(...)
+                local _47_, _48_ = ...
+                if ((_47_ == false) and (nil ~= _48_)) then
+                  local e = _48_
                   return e
                 else
-                  local __85_auto = _46_
+                  local __85_auto = _47_
                   return ...
                 end
               end
-              return _45_(loader)
+              return _46_(loader)
             else
-              local __85_auto = _44_
+              local __85_auto = _45_
               return ...
             end
           end
-          return _43_(record_loadfile(index))
+          return _44_(record_loadfile(index))
         else
-          local __85_auto = _42_
+          local __85_auto = _43_
           return ...
         end
       end
-      return _41_(make_module_record(modname0, src_path))
+      return _42_(make_module_record(modname0, src_path))
     else
-      local _ = _40_
+      local _ = _41_
       return false
     end
   end
   local function search_by_package_path(modname0)
     local search_package_path
     do
-      local _let_52_ = require("hotpot.searcher")
-      local mod_search = _let_52_["mod-search"]
-      local function _53_(modname1)
+      local _let_53_ = require("hotpot.searcher")
+      local mod_search = _let_53_["mod-search"]
+      local function _54_(modname1)
         return mod_search({prefix = "fnl", extension = "fnl", modnames = {(modname1 .. ".init"), modname1}, ["runtime-path?"] = false})
       end
-      search_package_path = _53_
+      search_package_path = _54_
     end
-    local _54_ = search_package_path(modname0)
-    if ((_G.type(_54_) == "table") and (nil ~= _54_[1])) then
-      local modpath = _54_[1]
-      local _let_55_ = require("hotpot.fennel")
-      local dofile = _let_55_["dofile"]
+    local _55_ = search_package_path(modname0)
+    if ((_G.type(_55_) == "table") and (nil ~= _55_[1])) then
+      local modpath = _55_[1]
+      local _let_56_ = require("hotpot.fennel")
+      local dofile = _let_56_["dofile"]
       vim.notify(fmt(("Found `%s` outside of Neovims RTP (at %s) by the package.path searcher.\n" .. "Hotpot will evaluate this file instead of compiling it."), modname0, modpath), vim.log.levels.NOTICE)
-      local function _56_()
+      local function _57_()
         return dofile(modpath)
       end
-      return _56_
+      return _57_
     else
-      local _ = _54_
+      local _ = _55_
       return false
     end
   end
-  local function _58_(...)
-    local _59_ = ...
-    if (_59_ == false) then
-      local function _60_(...)
-        local _61_ = ...
-        if (_61_ == false) then
-          local function _62_(...)
-            local _63_ = ...
-            if (_63_ == false) then
+  local function _59_(...)
+    local _60_ = ...
+    if (_60_ == false) then
+      local function _61_(...)
+        local _62_ = ...
+        if (_62_ == false) then
+          local function _63_(...)
+            local _64_ = ...
+            if (_64_ == false) then
               return nil
             else
-              local _3floader = _63_
+              local _3floader = _64_
               return _3floader
             end
           end
-          return _62_(search_by_package_path(modname))
+          return _63_(search_by_package_path(modname))
         else
-          local _3floader = _61_
+          local _3floader = _62_
           return _3floader
         end
       end
-      return _60_(search_by_rtp_fnl(modname))
+      return _61_(search_by_rtp_fnl(modname))
     else
-      local _3floader = _59_
+      local _3floader = _60_
       return _3floader
     end
   end
-  return _58_(search_by_existing_lua(modname))
+  return _59_(search_by_existing_lua(modname))
 end
 local function searcher(modname, ...)
   if not ("hotpot." == string.sub(modname, 1, 7)) then
@@ -264,7 +266,7 @@ local function searcher(modname, ...)
   end
 end
 local function make_record_loader(record)
-  _G.assert((nil ~= record), "Missing argument record on fnl/hotpot/loader/init.fnl:161")
+  _G.assert((nil ~= record), "Missing argument record on fnl/hotpot/loader/init.fnl:160")
   return record_loadfile(record)
 end
 return {searcher = searcher, ["compiled-cache-path"] = cache_path_for_compiled_artefact(), ["cache-path-for-compiled-artefact"] = cache_path_for_compiled_artefact, ["make-record-loader"] = make_record_loader}
