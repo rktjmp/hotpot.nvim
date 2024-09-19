d251354b5e26962c9a16059fe7a0a339c58b3463
Oliver Marriott <hello@omarriott.com>
2024-09-19 14:54:55 +1000

    chore: bump lua build

--- lua/hotpot/health.lua
+++ lua/hotpot/health.lua
@@ -1,12 +1,13 @@
 local uv = (vim.uv or vim.loop)
 local function _2_(...)
   local _1_ = vim.health
-  if ((_G.type(_1_) == "table") and (nil ~= _1_.ok) and (nil ~= _1_.info) and (nil ~= _1_.error) and (nil ~= _1_.start)) then
+  if ((_G.type(_1_) == "table") and (nil ~= _1_.ok) and (nil ~= _1_.info) and (nil ~= _1_.error) and (nil ~= _1_.start) and (nil ~= _1_.warn)) then
     local ok = _1_.ok
     local info = _1_.info
     local error = _1_.error
     local start = _1_.start
-    return {report_start = start, report_info = info, report_error = error, report_ok = ok}
+    local warn = _1_.warn
+    return {report_start = start, report_warn = warn, report_info = info, report_error = error, report_ok = ok}
   elseif (nil ~= _1_) then
     local other = _1_
     return other
@@ -19,6 +20,7 @@ local report_start = _local_4_["report_start"]
 local report_info = _local_4_["report_info"]
 local report_ok = _local_4_["report_ok"]
 local report_error = _local_4_["report_error"]
+local report_warn = _local_4_["report_warn"]
 local function fmt(s, ...)
   return string.format(s, ...)
 end
@@ -102,7 +104,7 @@ local function bytes__3ehuman(bytes)
   end
   return _5_({bytes, "%db"})
 end
-local function disk_info()
+local function disk_report()
   report_start("Hotpot Cache Data")
   local runtime = require("hotpot.runtime")
   local config = runtime["user-config"]()
@@ -130,7 +132,7 @@ local function disk_info()
   report_info(fmt("Cache root path: %s", cache_root))
   return report_info(fmt("Cache size: %s files, %s", count, size))
 end
-local function log_info()
+local function log_report()
   report_start("Hotpot Log")
   local logger = require("hotpot.logger")
   local path = logger.path()
@@ -149,38 +151,111 @@ local function log_info()
   report_info(fmt("Log path: %s", path))
   return report_info(fmt("Log size: %s", size))
 end
-local function searcher_info()
-  report_start("Hotpot Module Searcher")
-  local _let_29_ = require("hotpot.loader")
-  local searcher = _let_29_["searcher"]
-  local expected_index = 2
-  local actual_index
-  do
-    local x = nil
-    for i, v in ipairs(package.loaders) do
-      if x then break end
-      if (searcher == v) then
-        x = i
+local function find_searcher_index(searcher)
+  local x = nil
+  for i, v in ipairs(package.loaders) do
+    if x then break end
+    if (searcher == v) then
+      x = i
+    else
+      x = nil
+    end
+  end
+  return x
+end
+local function check_searcher_preload_then_hotpot(preloader_index, hotpot_index)
+  local function loader_func_is_preload_loader_3f(func)
+    local ok_3f = false
+    local modname = "hotpot-health-preload-check"
+    local function _30_()
+      print("hi")
+      ok_3f = true
+      return nil
+    end
+    package.preload[modname] = _30_
+    package.loaded[modname] = nil
+    do
+      local _31_, _32_ = pcall(func, "hotpot-health-preload-check")
+      if ((_31_ == true) and (nil ~= _32_)) then
+        local f = _32_
+        f()
       else
-        x = nil
       end
     end
-    actual_index = x
+    package.preload[modname] = nil
+    package.loaded[modname] = nil
+    return ok_3f
   end
-  if (expected_index == actual_index) then
-    return report_ok(fmt("package.loader index: %s", actual_index))
+  if loader_func_is_preload_loader_3f(package.loaders[preloader_index]) then
+    report_ok(fmt("Preload package.loader index: %s", preloader_index))
+    report_ok(fmt("Hotpot package.loader index: %s", hotpot_index))
+    return true
   else
-    report_error(fmt("package.loader index: %s, requires: %s", actual_index, expected_index))
+    report_warn(fmt("Unknown package.loader index: %s, may or may not interfere with Hotpot.", preloader_index))
+    report_warn(fmt("Hotpot package.loader index: %s", hotpot_index))
+    return false
+  end
+end
+local function searcher_report_when_luarocks(hotpot_searcher, luarocks_searcher)
+  report_info("Luarocks.loader is present.")
+  do
+    local _35_ = find_searcher_index(luarocks_searcher)
+    if (_35_ == 1) then
+      report_ok("Luarocks package.loader index: 1")
+    elseif (nil ~= _35_) then
+      local n = _35_
+      report_warn(fmt("Luarocks package.loader index: %s, expected 1", n))
+    else
+    end
+  end
+  local _37_ = find_searcher_index(hotpot_searcher)
+  if (_37_ == 2) then
+    return report_ok(fmt("Hotpot package.loader index: %s", 2))
+  elseif (_37_ == 3) then
+    return check_searcher_preload_then_hotpot(2, 3)
+  elseif (nil ~= _37_) then
+    local n = _37_
+    return report_error(fmt("Hotpot package.loader index: %s, expected 2 or 3 when using luarocks.", n))
+  else
+    return nil
+  end
+end
+local function searcher_report_when_normal(hotpot_searcher)
+  local _39_ = find_searcher_index(hotpot_searcher)
+  if (_39_ == 2) then
+    return check_searcher_preload_then_hotpot(1, 2)
+  elseif (nil ~= _39_) then
+    local n = _39_
+    report_error(fmt("Hotpot package.loader index: %s, expected 2.", n))
     if vim.loader.enabled then
       return report_info(fmt("Ensure you are calling `vim.loader.enable()` before `require('hotpot')`"))
     else
       return nil
     end
+  else
+    return nil
+  end
+end
+local function searcher_report()
+  report_start("Hotpot Module Searcher")
+  if vim.loader.enabled then
+    report_info("vim.loader is enabled.")
+  else
+  end
+  local _let_43_ = require("hotpot.loader")
+  local hotpot_searcher = _let_43_["searcher"]
+  local _44_ = package.loaded["luarocks.loader"]
+  if ((_G.type(_44_) == "table") and (nil ~= _44_.luarocks_loader)) then
+    local luarocks_searcher = _44_.luarocks_loader
+    return searcher_report_when_luarocks(hotpot_searcher, luarocks_searcher)
+  else
+    local _ = _44_
+    return searcher_report_when_normal(hotpot_searcher)
   end
 end
 local function check()
-  disk_info()
-  log_info()
-  return searcher_info()
+  disk_report()
+  log_report()
+  return searcher_report()
 end
 return {check = check}
