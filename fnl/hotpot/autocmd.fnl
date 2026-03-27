e99ab675081ada3e2e6ea67c3ecca17ab0975ce5
Oliver Marriott <hello@omarriott.com>
2026-03-27 11:39:40 +1100

    fix: dont treat context.nearest -> nil as an error in autocmd

--- fnl/hotpot/autocmd.fnl
+++ fnl/hotpot/autocmd.fnl
@@ -9,17 +9,16 @@
   ;; (vim.print event)
   (let [{: Context} R
         {:match path} event]
-    (case-try
-      (pcall Context.nearest path) (true root)
-      (pcall Context.new root) (true ctx)
-      (pcall Context.sync ctx) (true _report)
-      nil
-      (catch
-        (false err) (vim.notify  err vim.log.level.ERROR {})))
-    ; (case (Context.nearest path)
-    ;   root (case (Context.new root)
-    ;          ctx (Context.sync ctx)
-    ;          (nil err) (vim.notify  err vim.log.level.ERROR {})))
+    (case (Context.nearest path)
+      root (case-try
+             (pcall Context.new root) (true ctx)
+             (pcall Context.sync ctx) (true _report)
+             nil
+             (catch
+               (false err) (vim.notify  err vim.log.levels.ERROR {})))
+      ;; we may be saving just some random fennel file, so not finding a
+      ;; nearest context doesn't matter.
+      nil nil)
     ;; return nil to retain cmd
     nil))
 
@@ -39,5 +38,7 @@
     (vim.api.nvim_del_augroup_by_id *augroup-id*)
     (set *augroup-id* nil)))
 
+(fn M.enabled? []
+  (~= nil *augroup-id*))
 
 (values M)
--- lua/hotpot/autocmd.lua
+++ lua/hotpot/autocmd.lua
@@ -4,50 +4,46 @@ local M, m = {}, {}
 local function buf_write_post_callback(event)
   local Context = R.Context
   local path = event.match
-  local function _2_(...)
-    local case_3_, case_4_ = ...
-    if ((case_3_ == true) and (nil ~= case_4_)) then
-      local root = case_4_
-      local function _5_(...)
-        local case_6_, case_7_ = ...
-        if ((case_6_ == true) and (nil ~= case_7_)) then
-          local ctx = case_7_
-          local function _8_(...)
-            local case_9_, case_10_ = ...
-            if ((case_9_ == true) and true) then
-              local _report = case_10_
+  do
+    local case_2_ = Context.nearest(path)
+    if (nil ~= case_2_) then
+      local root = case_2_
+      local function _3_(...)
+        local case_4_, case_5_ = ...
+        if ((case_4_ == true) and (nil ~= case_5_)) then
+          local ctx = case_5_
+          local function _6_(...)
+            local case_7_, case_8_ = ...
+            if ((case_7_ == true) and true) then
+              local _report = case_8_
               return nil
-            elseif ((case_9_ == false) and (nil ~= case_10_)) then
-              local err = case_10_
-              return vim.notify(err, vim.log.level.ERROR, {})
+            elseif ((case_7_ == false) and (nil ~= case_8_)) then
+              local err = case_8_
+              return vim.notify(err, vim.log.levels.ERROR, {})
             else
               return nil
             end
           end
-          return _8_(pcall(Context.sync, ctx))
-        elseif ((case_6_ == false) and (nil ~= case_7_)) then
-          local err = case_7_
-          return vim.notify(err, vim.log.level.ERROR, {})
+          return _6_(pcall(Context.sync, ctx))
+        elseif ((case_4_ == false) and (nil ~= case_5_)) then
+          local err = case_5_
+          return vim.notify(err, vim.log.levels.ERROR, {})
         else
           return nil
         end
       end
-      return _5_(pcall(Context.new, root))
-    elseif ((case_3_ == false) and (nil ~= case_4_)) then
-      local err = case_4_
-      return vim.notify(err, vim.log.level.ERROR, {})
+      _3_(pcall(Context.new, root))
+    elseif (case_2_ == nil) then
     else
-      return nil
     end
   end
-  _2_(pcall(Context.nearest, path))
   return nil
 end
 local _2aaugroup_id_2a = nil
 M.enable = function()
   if not _2aaugroup_id_2a then
     local augroup_id = vim.api.nvim_create_augroup("hotpot-fnl-ft", {clear = true})
-    vim.api.nvim_create_autocmd({"BufWritePost"}, {pattern = {"*.fnl", "*.fnlm"}, callback = buf_write_post_callback})
+    vim.api.nvim_create_autocmd({"BufWritePost"}, {pattern = {"*.fnl", "*.fnlm"}, group = augroup_id, callback = buf_write_post_callback})
     _2aaugroup_id_2a = augroup_id
     return nil
   else
@@ -56,11 +52,14 @@ M.enable = function()
 end
 M.disable = function()
   if _2aaugroup_id_2a then
-    vim.api.nvim_delete_augroup_by_id(_2aaugroup_id_2a)
+    vim.api.nvim_del_augroup_by_id(_2aaugroup_id_2a)
     _2aaugroup_id_2a = nil
     return nil
   else
     return nil
   end
+end
+M["enabled?"] = function()
+  return (nil ~= _2aaugroup_id_2a)
 end
 return M
