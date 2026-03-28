bf7f242d732de29cab2471c96f4ebac3afe30c2b
Oliver Marriott <hello@omarriott.com>
2026-03-28 20:54:27 +1100

    feat: checkhealth

--- fnl/hotpot/health.fnl
+++ fnl/hotpot/health.fnl
@@ -1,123 +1,37 @@
-(local uv (or vim.uv vim.loop))
-(local {: report_start : report_info : report_ok : report_error : report_warn}
-  (case vim.health
-    ;; 0.10.0+
-    {: ok : info : error : start : warn} {:report_start start
-                                          :report_warn warn
-                                          :report_info info
-                                          :report_error error
-                                          :report_ok ok}
-    ;; 0.9.0...
-    other other))
+(local {: R : notify-error} (require :hotpot.util))
 
-(fn fmt [s ...] (string.format s ...))
+(fn context-report [root ctx err]
+  (vim.health.start (.. root " Context"))
+  (case ctx
+    ctx (do
+          (vim.health.info (string.format "target: {%s}" ctx.target))
+          (vim.health.info (string.format "source: `%s`" ctx.path.source))
+          (vim.health.info (string.format "destination: `%s`" ctx.path.dest)))
+    _ (do
+        (vim.health.error err))))
 
-(fn bytes->human [bytes]
-  (fn f [b] (/ b 1024))
-  (case-try
-    [bytes :%db] (where [bytes unit] (< 1023 bytes))
-    [(f bytes) :%dkb] (where [kbytes unit] (< 1023 kbytes))
-    [(f kbytes) :%.2fmb] (where [mbytes unit] (< 1023 mbytes))
-    [(f mbytes) :%.2fbg] [gbytes unit]
-    (fmt unit gbytes)
-    (catch
-      [size unit] (fmt unit size))))
+(fn fennel-update-report []
+  (vim.health.start ":Hotpot fennel update")
 
-(fn disk-report []
-  (report_start "Hotpot Cache Data")
-  (let [runtime (require :hotpot.runtime)
-        config (runtime.user-config)
-        cache-root (runtime.cache-root-path)
-        paths (vim.fn.globpath cache-root "**" true true true)
-        count (length paths)
-        size (-> (accumulate [size 0 _ p (ipairs paths)]
-                   (+ size (or (?. (uv.fs_stat p) :size) 0)))
-                 (bytes->human))]
-    (report_info (fmt "Cache root path: %s" cache-root))
-    (report_info (fmt "Cache size: %s files, %s" count size))))
+  (case (= 1 (vim.fn.executable :curl))
+    true (vim.health.ok "`curl` is executable")
+    false (vim.health.warn "`curl` is not executable" "Install curl to run `:Hotpot fennel update`"))
 
-(fn log-report []
-  (report_start "Hotpot Log")
-  (let [logger (require :hotpot.logger)
-        path (logger.path)
-        size (case (uv.fs_stat path)
-               nil 0
-               {: size} (bytes->human size))]
-    (report_info (fmt "Log path: %s" path))
-    (report_info (fmt "Log size: %s" size))))
-
-(fn find-searcher-index [searcher]
-  (accumulate [x nil i v (ipairs package.loaders) &until x]
-    (if (= searcher v) i)))
-
-(fn check-searcher-preload-then-hotpot [preloader-index hotpot-index]
-  (fn loader-func-is-preload-loader? [func]
-    (var ok? false)
-    (local modname :hotpot-health-preload-check)
-    ;; ensure clean state
-    (tset package.preload modname #(do
-                                     (print :hi)
-                                     (set ok? true)))
-    (tset package.loaded modname nil)
-    ;; Note this wont be 100% fool proof, a loader might internally check preload
-    (case (pcall func :hotpot-health-preload-check)
-      (true f) (f))
-    ;; clear lingering state
-    (tset package.preload modname nil)
-    (tset package.loaded modname nil)
-    ok?)
-
-  (if (loader-func-is-preload-loader? (. package.loaders preloader-index))
-    (do
-      (report_ok (fmt "Preload package.loader index: %s" preloader-index))
-      (report_ok (fmt "Hotpot package.loader index: %s" hotpot-index))
-      (values true))
-    (do
-      (report_warn (fmt "Unknown package.loader index: %s, may or may not interfere with Hotpot." preloader-index))
-      (report_warn (fmt "Hotpot package.loader index: %s" hotpot-index))
-      (values false))))
-
-(fn searcher-report-when-luarocks [hotpot-searcher luarocks-searcher]
-  (report_info "Luarocks.loader is present.")
-
-  (case (find-searcher-index luarocks-searcher)
-    1 (report_ok "Luarocks package.loader index: 1")
-    ;; This may not strictly be an error, if its after hotpot it wont matter,
-    ;; but as of 2024-09-19 luarocks@bec4a9c, luarocks always installs
-    ;; luarocks.loader at index 1.
-    n (report_warn (fmt "Luarocks package.loader index: %s, expected 1" n)))
-
-  ;; luarocks always installs at index 1, so depending on when we were setup,
-  ;; we might either install after it (thinking it is preload), or have been
-  ;; pushed back to index 3 (luarocks, preload, hotpot).
-  (case (find-searcher-index hotpot-searcher)
-    ;; Because of the above, we check package.preload ourselves, 2 is ok.
-    2 (report_ok (fmt "Hotpot package.loader index: %s" 2))
-    ;; 3 is also ok if 2 is just the preloader, or at least looks a lot like
-    ;; the preloader.
-    3 (check-searcher-preload-then-hotpot 2 3)
-    n (report_error (fmt "Hotpot package.loader index: %s, expected 2 or 3 when using luarocks." n))))
-
-(fn searcher-report-when-normal [hotpot-searcher]
-  (case (find-searcher-index hotpot-searcher)
-    2 (check-searcher-preload-then-hotpot 1 2)
-    n (do
-        (report_error (fmt "Hotpot package.loader index: %s, expected 2." n))
-        (if vim.loader.enabled
-          (report_info (fmt "Ensure you are calling `vim.loader.enable()` before `require('hotpot')`"))))))
-
-(fn searcher-report []
-  (report_start "Hotpot Module Searcher")
-  (if vim.loader.enabled (report_info "vim.loader is enabled."))
-  (let [{:searcher hotpot-searcher} (require :hotpot.loader)]
-    (case (. package.loaded :luarocks.loader)
-      {:luarocks_loader luarocks-searcher} (searcher-report-when-luarocks hotpot-searcher luarocks-searcher)
-      ;; TODO: possibly expand this for when lazy?
-      _ (searcher-report-when-normal hotpot-searcher))))
+  (vim.health.start ":Hotpot fennel version")
+  (case (pcall require :hotpot.update-fennel.fennel)
+    (true mod) (vim.health.info (string.format "Using custom Fennel version: `%s`" mod.version))
+    false (vim.health.info (string.format "Using default Fennel version: `%s`" R.fennel.version))))
 
 (fn check []
-  (disk-report)
-  (log-report)
-  (searcher-report))
+  (let [config (vim.fn.stdpath :config)
+        ctx (R.context.new config)]
+    (context-report config ctx))
+
+  (case (R.context.nearest (vim.uv.cwd))
+    root (case (pcall R.context.new root)
+           (true ctx) (context-report root ctx)
+           (nil err) (context-report root nil err)))
+
+  (fennel-update-report))
 
 {: check}
--- lua/hotpot/health.lua
+++ lua/hotpot/health.lua
@@ -1,261 +1,62 @@
-local uv = (vim.uv or vim.loop)
-local function _2_(...)
-  local case_1_ = vim.health
-  if ((_G.type(case_1_) == "table") and (nil ~= case_1_.ok) and (nil ~= case_1_.info) and (nil ~= case_1_.error) and (nil ~= case_1_.start) and (nil ~= case_1_.warn)) then
-    local ok = case_1_.ok
-    local info = case_1_.info
-    local error = case_1_.error
-    local start = case_1_.start
-    local warn = case_1_.warn
-    return {report_start = start, report_warn = warn, report_info = info, report_error = error, report_ok = ok}
-  elseif (nil ~= case_1_) then
-    local other = case_1_
-    return other
+local _local_1_ = require("hotpot.util")
+local R = _local_1_.R
+local notify_error = _local_1_["notify-error"]
+local function context_report(root, ctx, err)
+  vim.health.start((root .. " Context"))
+  if (nil ~= ctx) then
+    local ctx0 = ctx
+    vim.health.info(string.format("target: {%s}", ctx0.target))
+    vim.health.info(string.format("source: `%s`", ctx0.path.source))
+    return vim.health.info(string.format("destination: `%s`", ctx0.path.dest))
   else
-    return nil
+    local _ = ctx
+    return vim.health.error(err)
   end
 end
-local _local_4_ = _2_(...)
-local report_start = _local_4_.report_start
-local report_info = _local_4_.report_info
-local report_ok = _local_4_.report_ok
-local report_error = _local_4_.report_error
-local report_warn = _local_4_.report_warn
-local function fmt(s, ...)
-  return string.format(s, ...)
-end
-local function bytes__3ehuman(bytes)
-  local function f(b)
-    return (b / 1024)
-  end
-  local function _5_(...)
-    local case_6_ = ...
-    local and_7_ = ((_G.type(case_6_) == "table") and (nil ~= case_6_[1]) and (nil ~= case_6_[2]))
-    if and_7_ then
-      local bytes0 = case_6_[1]
-      local unit = case_6_[2]
-      and_7_ = (1023 < bytes0)
-    end
-    if and_7_ then
-      local bytes0 = case_6_[1]
-      local unit = case_6_[2]
-      local function _9_(...)
-        local case_10_ = ...
-        local and_11_ = ((_G.type(case_10_) == "table") and (nil ~= case_10_[1]) and (nil ~= case_10_[2]))
-        if and_11_ then
-          local kbytes = case_10_[1]
-          local unit0 = case_10_[2]
-          and_11_ = (1023 < kbytes)
-        end
-        if and_11_ then
-          local kbytes = case_10_[1]
-          local unit0 = case_10_[2]
-          local function _13_(...)
-            local case_14_ = ...
-            local and_15_ = ((_G.type(case_14_) == "table") and (nil ~= case_14_[1]) and (nil ~= case_14_[2]))
-            if and_15_ then
-              local mbytes = case_14_[1]
-              local unit1 = case_14_[2]
-              and_15_ = (1023 < mbytes)
-            end
-            if and_15_ then
-              local mbytes = case_14_[1]
-              local unit1 = case_14_[2]
-              local function _17_(...)
-                local case_18_ = ...
-                if ((_G.type(case_18_) == "table") and (nil ~= case_18_[1]) and (nil ~= case_18_[2])) then
-                  local gbytes = case_18_[1]
-                  local unit2 = case_18_[2]
-                  return fmt(unit2, gbytes)
-                elseif ((_G.type(case_18_) == "table") and (nil ~= case_18_[1]) and (nil ~= case_18_[2])) then
-                  local size = case_18_[1]
-                  local unit2 = case_18_[2]
-                  return fmt(unit2, size)
-                else
-                  return nil
-                end
-              end
-              return _17_({f(mbytes), "%.2fbg"})
-            elseif ((_G.type(case_14_) == "table") and (nil ~= case_14_[1]) and (nil ~= case_14_[2])) then
-              local size = case_14_[1]
-              local unit1 = case_14_[2]
-              return fmt(unit1, size)
-            else
-              return nil
-            end
-          end
-          return _13_({f(kbytes), "%.2fmb"})
-        elseif ((_G.type(case_10_) == "table") and (nil ~= case_10_[1]) and (nil ~= case_10_[2])) then
-          local size = case_10_[1]
-          local unit0 = case_10_[2]
-          return fmt(unit0, size)
-        else
-          return nil
-        end
-      end
-      return _9_({f(bytes0), "%dkb"})
-    elseif ((_G.type(case_6_) == "table") and (nil ~= case_6_[1]) and (nil ~= case_6_[2])) then
-      local size = case_6_[1]
-      local unit = case_6_[2]
-      return fmt(unit, size)
-    else
-      return nil
-    end
-  end
-  return _5_({bytes, "%db"})
-end
-local function disk_report()
-  report_start("Hotpot Cache Data")
-  local runtime = require("hotpot.runtime")
-  local config = runtime["user-config"]()
-  local cache_root = runtime["cache-root-path"]()
-  local paths = vim.fn.globpath(cache_root, "**", true, true, true)
-  local count = #paths
-  local size
-  local function _23_()
-    local size0 = 0
-    for _, p in ipairs(paths) do
-      local _25_
-      do
-        local t_24_ = uv.fs_stat(p)
-        if (nil ~= t_24_) then
-          t_24_ = t_24_.size
-        else
-        end
-        _25_ = t_24_
-      end
-      size0 = (size0 + (_25_ or 0))
-    end
-    return size0
-  end
-  size = bytes__3ehuman(_23_())
-  report_info(fmt("Cache root path: %s", cache_root))
-  return report_info(fmt("Cache size: %s files, %s", count, size))
-end
-local function log_report()
-  report_start("Hotpot Log")
-  local logger = require("hotpot.logger")
-  local path = logger.path()
-  local size
+local function fennel_update_report()
+  vim.health.start(":Hotpot fennel update")
   do
-    local case_27_ = uv.fs_stat(path)
-    if (case_27_ == nil) then
-      size = 0
-    elseif ((_G.type(case_27_) == "table") and (nil ~= case_27_.size)) then
-      local size0 = case_27_.size
-      size = bytes__3ehuman(size0)
-    else
-      size = nil
-    end
-  end
-  report_info(fmt("Log path: %s", path))
-  return report_info(fmt("Log size: %s", size))
-end
-local function find_searcher_index(searcher)
-  local x = nil
-  for i, v in ipairs(package.loaders) do
-    if x then break end
-    if (searcher == v) then
-      x = i
-    else
-      x = nil
-    end
-  end
-  return x
-end
-local function check_searcher_preload_then_hotpot(preloader_index, hotpot_index)
-  local function loader_func_is_preload_loader_3f(func)
-    local ok_3f = false
-    local modname = "hotpot-health-preload-check"
-    local function _30_()
-      print("hi")
-      ok_3f = true
-      return nil
-    end
-    package.preload[modname] = _30_
-    package.loaded[modname] = nil
-    do
-      local case_31_, case_32_ = pcall(func, "hotpot-health-preload-check")
-      if ((case_31_ == true) and (nil ~= case_32_)) then
-        local f = case_32_
-        f()
-      else
-      end
-    end
-    package.preload[modname] = nil
-    package.loaded[modname] = nil
-    return ok_3f
-  end
-  if loader_func_is_preload_loader_3f(package.loaders[preloader_index]) then
-    report_ok(fmt("Preload package.loader index: %s", preloader_index))
-    report_ok(fmt("Hotpot package.loader index: %s", hotpot_index))
-    return true
-  else
-    report_warn(fmt("Unknown package.loader index: %s, may or may not interfere with Hotpot.", preloader_index))
-    report_warn(fmt("Hotpot package.loader index: %s", hotpot_index))
-    return false
-  end
-end
-local function searcher_report_when_luarocks(hotpot_searcher, luarocks_searcher)
-  report_info("Luarocks.loader is present.")
-  do
-    local case_35_ = find_searcher_index(luarocks_searcher)
-    if (case_35_ == 1) then
-      report_ok("Luarocks package.loader index: 1")
-    elseif (nil ~= case_35_) then
-      local n = case_35_
-      report_warn(fmt("Luarocks package.loader index: %s, expected 1", n))
+    local case_3_ = (1 == vim.fn.executable("curl"))
+    if (case_3_ == true) then
+      vim.health.ok("`curl` is executable")
+    elseif (case_3_ == false) then
+      vim.health.warn("`curl` is not executable", "Install curl to run `:Hotpot fennel update`")
     else
     end
   end
-  local case_37_ = find_searcher_index(hotpot_searcher)
-  if (case_37_ == 2) then
-    return report_ok(fmt("Hotpot package.loader index: %s", 2))
-  elseif (case_37_ == 3) then
-    return check_searcher_preload_then_hotpot(2, 3)
-  elseif (nil ~= case_37_) then
-    local n = case_37_
-    return report_error(fmt("Hotpot package.loader index: %s, expected 2 or 3 when using luarocks.", n))
+  vim.health.start(":Hotpot fennel version")
+  local case_5_, case_6_ = pcall(require, "hotpot.update-fennel.fennel")
+  if ((case_5_ == true) and (nil ~= case_6_)) then
+    local mod = case_6_
+    return vim.health.info(string.format("Using custom Fennel version: `%s`", mod.version))
+  elseif (case_5_ == false) then
+    return vim.health.info(string.format("Using default Fennel version: `%s`", R.fennel.version))
   else
     return nil
-  end
-end
-local function searcher_report_when_normal(hotpot_searcher)
-  local case_39_ = find_searcher_index(hotpot_searcher)
-  if (case_39_ == 2) then
-    return check_searcher_preload_then_hotpot(1, 2)
-  elseif (nil ~= case_39_) then
-    local n = case_39_
-    report_error(fmt("Hotpot package.loader index: %s, expected 2.", n))
-    if vim.loader.enabled then
-      return report_info(fmt("Ensure you are calling `vim.loader.enable()` before `require('hotpot')`"))
-    else
-      return nil
-    end
-  else
-    return nil
-  end
-end
-local function searcher_report()
-  report_start("Hotpot Module Searcher")
-  if vim.loader.enabled then
-    report_info("vim.loader is enabled.")
-  else
-  end
-  local _let_43_ = require("hotpot.loader")
-  local hotpot_searcher = _let_43_.searcher
-  local case_44_ = package.loaded["luarocks.loader"]
-  if ((_G.type(case_44_) == "table") and (nil ~= case_44_.luarocks_loader)) then
-    local luarocks_searcher = case_44_.luarocks_loader
-    return searcher_report_when_luarocks(hotpot_searcher, luarocks_searcher)
-  else
-    local _ = case_44_
-    return searcher_report_when_normal(hotpot_searcher)
   end
 end
 local function check()
-  disk_report()
-  log_report()
-  return searcher_report()
+  do
+    local config = vim.fn.stdpath("config")
+    local ctx = R.context.new(config)
+    context_report(config, ctx)
+  end
+  do
+    local case_8_ = R.context.nearest(vim.uv.cwd())
+    if (nil ~= case_8_) then
+      local root = case_8_
+      local case_9_, case_10_ = pcall(R.context.new, root)
+      if ((case_9_ == true) and (nil ~= case_10_)) then
+        local ctx = case_10_
+        context_report(root, ctx)
+      elseif ((case_9_ == nil) and (nil ~= case_10_)) then
+        local err = case_10_
+        context_report(root, nil, err)
+      else
+      end
+    else
+    end
+  end
+  return fennel_update_report()
 end
 return {check = check}
